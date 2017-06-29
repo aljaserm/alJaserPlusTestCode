fcdf9c79b59a0cbd174d244820e26783b53c9a9b
mohammed aljaser <mohammed.aljaser7@hotmail.com>
2017-06-29 03:39:04 -0400

    Add project files.

---
+++ alJaserPlusTestCode/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Web.Optimization;
+
+namespace alJaserPlusTestCode
+{
+        public class BundleConfig
+        {
+            public static void RegisterBundles(BundleCollection bundles)
+            {
+                bundles.Add(new StyleBundle("~/Content/css").Include(
+                          "~/Content/site.css"));
+
+                BundleTable.EnableOptimizations = true;
+            }
+        }
+}
